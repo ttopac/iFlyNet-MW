e62c9ad30d70248e417cd07ceac02d5caff58df3
Tanay Topac <tanaytopac@stanford.edu>
2020-08-27 16:02:58 -0700

    added change for new sensors.

--- fbf-DAQ/acquire_data.py
+++ fbf-DAQ/acquire_data.py
@@ -7,11 +7,21 @@ import numpy as np
 
 def calibrate_SGs(params):
   with nidaqmx.Task() as task:
+    task.ai_channels.add_ai_voltage_chan("cDAQ1Mod2/ai2") #SG_1
+    task.ai_channels.add_ai_voltage_chan("cDAQ1Mod2/ai3") #SG_2
+    task.ai_channels.add_ai_voltage_chan("cDAQ1Mod3/ai0") #SG_3
+    task.ai_channels.add_ai_voltage_chan("cDAQ1Mod3/ai1") #SG_4
+    task.ai_channels.add_ai_voltage_chan("cDAQ1Mod3/ai2") #SG_5
+    task.ai_channels.add_ai_voltage_chan("cDAQ1Mod3/ai3") #SG_6
+    task.ai_channels.add_ai_voltage_chan("cDAQ1Mod4/ai0") #SG_7
+    task.ai_channels.add_ai_voltage_chan("cDAQ1Mod4/ai2") #SG_9
     task.ai_channels.add_ai_strain_gage_chan("cDAQ1Mod8/ai0", strain_config=StrainGageBridgeType.QUARTER_BRIDGE_I, voltage_excit_val=3.3, nominal_gage_resistance=351.4) #Lift
     task.ai_channels.add_ai_strain_gage_chan("cDAQ1Mod8/ai1", strain_config=StrainGageBridgeType.QUARTER_BRIDGE_I, voltage_excit_val=3.3, nominal_gage_resistance=351.4) #Drag
-    task.timing.cfg_samp_clk_timing(rate=params["sample_rate"], sample_mode=AcquisitionType.CONTINUOUS, samps_per_chan=params["sample_rate"])
-    sgsamples = np.asarray(task.read(number_of_samples_per_channel=params["samples_read"]))
-    sgmean = np.mean(sgsamples)
+    task.timing.cfg_samp_clk_timing(rate=params["sample_rate"], sample_mode=AcquisitionType.FINITE, samps_per_chan=params["sample_rate"])
+
+    calib_samples = np.zeros((10, params["sample_rate"]))
+    num_samples = task.read_many_sample(calib_samples, number_of_samples_per_channel=nidaqmx.constants.READ_ALL_AVAILABLE)
+    sgmean = np.mean(calib_samples, axis=1)
     print(sgmean)
     return sgmean
 
@@ -27,29 +37,33 @@ def get_data(aoa, vel, sgmean, params):
   with nidaqmx.Task() as task:
     task.ai_channels.add_ai_voltage_chan("cDAQ1Mod1/ai0") #PZT_1
     task.ai_channels.add_ai_voltage_chan("cDAQ1Mod1/ai1") #PZT_2
-    task.ai_channels.add_ai_voltage_chan("cDAQ1Mod6/ai2") #PZT_3
+    task.ai_channels.add_ai_voltage_chan("cDAQ1Mod1/ai2") #PZT_3
+    task.ai_channels.add_ai_voltage_chan("cDAQ1Mod1/ai3") #PZT_4
+    task.ai_channels.add_ai_voltage_chan("cDAQ1Mod2/ai0") #PZT_5
+    task.ai_channels.add_ai_voltage_chan("cDAQ1Mod2/ai1") #PZT_6
+    task.ai_channels.add_ai_voltage_chan("cDAQ1Mod2/ai2") #SG_1
+    task.ai_channels.add_ai_voltage_chan("cDAQ1Mod2/ai3") #SG_2
+    task.ai_channels.add_ai_voltage_chan("cDAQ1Mod3/ai0") #SG_3
+    task.ai_channels.add_ai_voltage_chan("cDAQ1Mod3/ai1") #SG_4
+    task.ai_channels.add_ai_voltage_chan("cDAQ1Mod3/ai2") #SG_5
+    task.ai_channels.add_ai_voltage_chan("cDAQ1Mod3/ai3") #SG_6
+    task.ai_channels.add_ai_voltage_chan("cDAQ1Mod4/ai0") #SG_7
+    task.ai_channels.add_ai_voltage_chan("cDAQ1Mod4/ai2") #SG_9
+    task.ai_channels.add_ai_strain_gage_chan("cDAQ1Mod8/ai0", strain_config=StrainGageBridgeType.QUARTER_BRIDGE_I, voltage_excit_val=3.3, nominal_gage_resistance=351.4) #Lift
+    task.ai_channels.add_ai_strain_gage_chan("cDAQ1Mod8/ai1", strain_config=StrainGageBridgeType.QUARTER_BRIDGE_I, voltage_excit_val=3.3, nominal_gage_resistance=351.4) #Drag
+    task.timing.cfg_samp_clk_timing(rate=params["sample_rate"], sample_mode=AcquisitionType.FINITE, samps_per_chan=params["samples_read_train"])
     
-    task.timing.cfg_samp_clk_timing(rate=params["sample_rate"], sample_mode=AcquisitionType.CONTINUOUS, samps_per_chan=params["sample_rate"])
-    testdata = np.zeros((4,1))
-    
-    while True:
-      data = np.asarray(task.read(number_of_samples_per_channel=params["samples_read"]))
-      data[3,:] -= sgmean
-      data[3,:] *= 1000000
-      testdata = np.concatenate((testdata,data),axis=1)
-      t1 = time.time()
-      if t1-t0 > 60:
-        testdata = testdata[:,0:params["sample_rate"]*60]
-        np.save('g:/Shared drives/WindTunnelTests-Feb2019/Nov2019_Tests/Baseline_Tests/sr=7000_str=2800_{}ms_{}deg.npy'.format(vel,aoa),testdata)
-        return aoa, vel
+    train_samples = np.zeros((16, params["samples_read_train"]))
+    num_samples = task.read_many_sample(train_samples, number_of_samples_per_channel=nidaqmx.constants.READ_ALL_AVAILABLE)
+    np.save('g:/Shared drives/WindTunnelTests-Feb2019/Sept2020_Tests/Training_Tests/train_{}ms_{}deg.npy'.format(vel,aoa),train_samples)
+    return aoa, vel
 
 
 if __name__ == "__main__":
   params = dict()
-  params["samples_read"] = 2800
   params["sample_rate"] = 7000
+  params["samples_read_train"] = 100000
   aoa, vel = 99, 99
-  counter = 0
 
   sgmean = calibrate_SGs(params)
   while True:
