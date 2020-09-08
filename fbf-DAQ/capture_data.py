1d088aa06e86489b75df72effe8a5912405ce646
tackoo <tanaytopac@gmail.com>
2020-09-07 19:22:16 -0700

    another attempt at fixing plot_realtime.

--- fbf-DAQ/capture_data.py
+++ fbf-DAQ/capture_data.py
@@ -69,14 +69,13 @@ def capture_data_continuous(SGoffsets, sample_rate, samples_to_read, queue):
     print ("DAQ sampling rate will be: {}".format(task.timing.samp_clk_rate))
 
     while True:
-      while queue.empty():
-        reader.read_many_sample(read_data, number_of_samples_per_channel=samples_to_read, timeout=nidaqmx.constants.WAIT_INFINITELY)
-        # read_data[6:14] = -(4*read_data[6:14]/SGcoeffs["amplifier_coeff"]) / (2*read_data[6:14]/SGcoeffs["amplifier_coeff"]*SGcoeffs["GF"] + SGcoeffs["Vex"]*SGcoeffs["GF"])
-        read_data[6:,:] -= SGoffsets.reshape(SGoffsets.shape[0],-1) #Subtract the offset to obtain calibrated data
-        read_data[14:,:] *= -1000000 #Convert only commercial SGs to microstrains with correct sign, leave our SGs in volts.
-        # print ("Data read by capture function: ", end="")
-        # print (np.mean(read_data[13]))
-        queue.put(read_data)
+      while queue.qsize() > 1: #This is here to keep up with delay in DAQ.
+        a = queue.get_nowait()
+      reader.read_many_sample(read_data, number_of_samples_per_channel=samples_to_read, timeout=nidaqmx.constants.WAIT_INFINITELY)
+      # read_data[6:14] = -(4*read_data[6:14]/SGcoeffs["amplifier_coeff"]) / (2*read_data[6:14]/SGcoeffs["amplifier_coeff"]*SGcoeffs["GF"] + SGcoeffs["Vex"]*SGcoeffs["GF"])
+      read_data[6:,:] -= SGoffsets.reshape(SGoffsets.shape[0],-1) #Subtract the offset to obtain calibrated data
+      read_data[14:,:] *= -1000000 #Convert only commercial SGs to microstrains with correct sign, leave our SGs in volts.
+      queue.put_nowait(read_data)
 
 
 
--- fbf-plot_preproc/plot_realtime.py
+++ fbf-plot_preproc/plot_realtime.py
@@ -3,7 +3,8 @@ import numpy as np
 import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.animation import FuncAnimation
-from multiprocessing import Process, Queue, Pipe
+from threading import Thread
+from multiprocessing import Process, Queue
 
 import sys
 import os
@@ -22,38 +23,34 @@ SGcoeffs["amplifier_coeff"] = 100
 SGcoeffs["GF"] = 2.11
 SGcoeffs["Vex"] = 12
 
-#Plotting coefficients
+#Plotting coefficients and initializer
 visible_duration = 30 #seconds
 plot_refresh_rate = 1 #seconds
 downsample_mult = 1
+ys = np.zeros((16,int(visible_duration*params["sample_rate"]/downsample_mult)))
+
 
 #Initialize the GUI
 root = tk.Tk()
 root.title ("Real-time Plotting")
 
-#Run animation
-def run_animate(ys, q2):
-  realtime_plot = PlotRealtime(params, visible_duration, plot_refresh_rate, downsample_mult)
-  canvas = FigureCanvasTkAgg(realtime_plot.fig, master=root)
-  canvas.get_tk_widget().grid(column=0, row=1)
-  ani = FuncAnimation(realtime_plot.fig, realtime_plot.plot_live, fargs=(ys,q2), interval=plot_refresh_rate*1000, blit=True)
-  root.mainloop()
-
 if __name__ == "__main__":
   # Get strain gauge offsets for calibration
   q1 = Queue()
-  p1 = Process(target = send_SG_offsets, args=(params["sample_rate"], int(params["sample_rate"]), q1))
+  p1 = Thread(target = send_SG_offsets, args=(params["sample_rate"], int(params["sample_rate"]), q1))
   p1.start()
   SGoffsets = q1.get()
   p1.join()
 
   # Run capture data in background
   q2 = Queue()
-  p1 = Process(target = send_data, args=(SGoffsets, params["sample_rate"], int(params["sample_rate"]*plot_refresh_rate), "continuous", q2))
-  p1.start()
+  p2 = Process(target = send_data, args=(SGoffsets, params["sample_rate"], int(params["sample_rate"]*plot_refresh_rate), "continuous", q2))
+  p2.start()
 
   # Plot the data
-  ys = np.zeros((16,int(visible_duration*params["sample_rate"]/downsample_mult)))
-  p2 = Process(target = run_animate, args=(ys,q2))
-  p2.start()
+  realtime_plot = PlotRealtime(params, visible_duration, plot_refresh_rate, downsample_mult)
+  canvas = FigureCanvasTkAgg(realtime_plot.fig, master=root)
+  canvas.get_tk_widget().grid(column=0, row=1)
+  ani = FuncAnimation(realtime_plot.fig, realtime_plot.plot_live, fargs=(ys,q2), interval=plot_refresh_rate*1000, blit=True)
+  root.mainloop()
   
