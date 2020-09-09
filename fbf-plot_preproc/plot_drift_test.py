7ba513777f36ba9ca993bd067d370bb88a8d0691
Tanay Topac <tanaytopac@stanford.edu>
2020-09-09 12:18:37 -0700

    added temperature compensation for CommSGs

--- fbf-plot_preproc/plot_drift_test.py
+++ fbf-plot_preproc/plot_drift_test.py
@@ -2,6 +2,10 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pandas as pd
+import sys, os
+sys.path.append(os.path.abspath('./helpers'))
+from proc_tempcomp_helper import CommSG_Temp_Comp
+
 
 #Plotting stuff. Seems OK to ignore.
 # def make_patch_spines_invisible(ax):
@@ -18,6 +22,12 @@ test_folder = 'drift4_Sept8'
 downsample_mult = 1700 #1700 is close to 1 datapoint per second since sampling rate is 1724.1379310344828 for drift test
 plot_anemo_temp = True
 
+plot_commSG_comp = True
+poly_coeffs = (-23.65, 2.06, -5.02E-2, 2.26E-4, 0.3, 0.219)
+gage_fact, k_poly = 2, 2
+gage_fact_CTE, SG_matl_CTE = 93E-6, 10.8E-6
+al6061_CTE = 23.6E-6
+
 # driftData = np.load('g:/Shared drives/WindTunnelTests-Feb2019/Sept2020_Tests/Training_Tests/{}/drifttest_{}ms_{}deg.npy'.format(test_folder,vel,aoa))
 driftData = np.load('/Volumes/GoogleDrive/Shared drives/WindTunnelTests-Feb2019/Sept2020_Tests/Training_Tests/{}/drifttest_{}ms_{}deg.npy'.format(test_folder,vel,aoa))
 downsampled_SSNSGs = np.mean (-driftData[6:14,:].reshape(8,-1,downsample_mult), axis=2) #Downsample the sensor network SG data
@@ -27,8 +37,8 @@ if plot_anemo_temp:
   # tempdata = 'g:/Shared drives/WindTunnelTests-Feb2019/Sept2020_Tests/Training_Tests/{}/drifttesttemp_{}ms_{}deg.npy'.format(test_folder,vel,aoa)
   tempdata = '/Volumes/GoogleDrive/Shared drives/WindTunnelTests-Feb2019/Sept2020_Tests/Training_Tests/{}/drifttesttemp_{}ms_{}deg.txt'.format(test_folder,vel,aoa)
   df = pd.read_csv(tempdata, header=0, delim_whitespace=True)
-  vel_np = df['Speed'].to_numpy()
-  temp_np_F = df['Temp.'].to_numpy()
+  vel_np = df['Speed'].to_numpy()[0:downsampled_commSG.shape[1]]
+  temp_np_F = df['Temp.'].to_numpy()[0:downsampled_commSG.shape[1]]
   temp_np_C = (temp_np_F-32) * 5 / 9
 
 fig = plt.figure(figsize=(12.0, 6.0))
@@ -50,14 +60,11 @@ ax2.plot(xs, downsampled_commSG[1], linewidth=0.5, label="SG Drag")
 ax1.set_title("-SSN SG readings for V = {}m/s, AoA = {}deg".format(vel,aoa), fontsize=12)
 ax1.set_xlabel("Time (min)", fontsize=11)
 ax1.set_ylabel("Voltage (V)", fontsize=11)
-ax1.legend(fontsize=7, loc="upper right", ncol=3, columnspacing=1)
 ax1.tick_params(labelsize="small")
 ax1.grid(False)
-
 ax2.set_title("Commercial SG readings for V = {}m/s, AoA = {}deg".format(vel,aoa), fontsize=12)
 ax2.set_xlabel("Time (min)", fontsize=11)
 ax2.set_ylabel("Microstrain (us)", fontsize=11)
-ax2.legend(fontsize=7, loc="upper right", ncol=1, columnspacing=1)
 ax2.tick_params(labelsize="small")
 ax2.grid(False)
 
@@ -65,8 +72,8 @@ if plot_anemo_temp:
   ax1_veltwin = ax1.twinx()
   ax1_temptwin = ax1.twinx()
   ax1_temptwin.spines["right"].set_position(("axes", 1.08))
-  ax1_veltwin.plot (xs, vel_np[0:downsampled_commSG.shape[1]], "b-", linewidth=0.8,  label="WT Speed")
-  ax1_temptwin.plot (xs, temp_np_C[0:downsampled_commSG.shape[1]], "r-", linewidth=0.8,  label="WT Temp")
+  ax1_veltwin.plot (xs, vel_np, "b-", linewidth=0.8,  label="WT Speed")
+  ax1_temptwin.plot (xs, temp_np_C, "r-", linewidth=0.8,  label="WT Temp")
   ax1_veltwin.set_ylabel("Airspeed (m/s)", fontsize=11)
   ax1_temptwin.set_ylabel("Temperature (C)", fontsize=11)
   ax1_veltwin.yaxis.label.set_color('b')
@@ -80,8 +87,8 @@ if plot_anemo_temp:
   ax2_veltwin = ax2.twinx()
   ax2_temptwin = ax2.twinx()
   ax2_temptwin.spines["right"].set_position(("axes", 1.08))
-  ax2_veltwin.plot (xs, vel_np[0:downsampled_commSG.shape[1]], "b-", linewidth=0.8,  label="WT Speed")
-  ax2_temptwin.plot (xs, temp_np_C[0:downsampled_commSG.shape[1]], "r-", linewidth=0.8,  label="WT Temp")
+  ax2_veltwin.plot (xs, vel_np, "b-", linewidth=0.8,  label="WT Speed")
+  ax2_temptwin.plot (xs, temp_np_C, "r-", linewidth=0.8,  label="WT Temp")
   ax2_veltwin.set_ylabel("Airspeed (m/s)", fontsize=11)
   ax2_temptwin.set_ylabel("Temperature (C)", fontsize=11)
   ax2_veltwin.yaxis.label.set_color('b')
@@ -92,5 +99,14 @@ if plot_anemo_temp:
   ax2_temptwin.grid(False)
   ax2_veltwin.grid(False)
 
+if plot_commSG_comp:
+  ref_temp = temp_np_C[0]
+  commSG_temp_comp = CommSG_Temp_Comp(poly_coeffs, gage_fact_CTE, SG_matl_CTE, al6061_CTE, ref_temp, gage_fact, k_poly)
+  comp_downsampled_commSG, comp_commSG_var = commSG_temp_comp.compensate(downsampled_commSG, temp_np_C)
+  ax2.plot(xs, -comp_downsampled_commSG[0], ':', color=ax2.lines[0].get_color(), linewidth=0.5, label="SG Lift (compensated)")
+  ax2.plot(xs, -comp_downsampled_commSG[1], ':', color=ax2.lines[1].get_color(), linewidth=0.5, label="SG Drag (compensated)")
+
+ax1.legend(fontsize=7, loc="upper right", ncol=3, columnspacing=1)
+ax2.legend(fontsize=7, loc="upper right", ncol=1, columnspacing=1)
 plt.tight_layout(pad=2.0)
 plt.show()
---
+++ helpers/proc_tempcomp_helper.py
@@ -0,0 +1,31 @@
+class CommSG_Temp_Comp():
+  def __init__ (self, poly_coeffs, gage_fact_CTE, SG_matl_CTE, substrate_matl_CTE, ref_temp, gage_fact=2, k_poly=2):
+    self.poly_coeffs = poly_coeffs
+    self.gage_fact_CTE = gage_fact_CTE
+    self.gage_fact = gage_fact
+    self.k_poly = k_poly
+    self.SG_matl_CTE = SG_matl_CTE
+    self.substrate_matl_CTE = substrate_matl_CTE
+    self.ref_temp = ref_temp
+
+  def comp_steel (self, temp):
+    e_therm = self.poly_coeffs[0] + self.poly_coeffs[1]*temp + self.poly_coeffs[2]*temp**2 + self.poly_coeffs[3]*temp**3
+    uncert = (temp - self.ref_temp) * self.poly_coeffs[4]
+    lead_influence = (temp - self.ref_temp) * self.poly_coeffs[5]
+    return (e_therm, uncert, lead_influence)
+
+  def correct_gage_fact (self, temp):
+    return self.gage_fact + self.gage_fact_CTE*(temp - self.ref_temp)
+
+  def comp_matl_dev (self, temp):
+    return (self.substrate_matl_CTE - self.SG_matl_CTE)*(temp - self.ref_temp)*1E6
+
+  def compensate (self, readings, temp):
+    e_therm, uncert, lead_influence = self.comp_steel(temp)
+    corr_gage_fact = self.correct_gage_fact(temp)
+    corr_matl_dev = self.comp_matl_dev(temp)
+    e_steel = -1*readings*self.gage_fact/corr_gage_fact - (e_therm*self.k_poly/self.gage_fact) - lead_influence
+    e_substrate = e_steel - corr_matl_dev
+    return (e_substrate, uncert)
+
+  
