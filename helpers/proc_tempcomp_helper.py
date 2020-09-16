ad290a30fb56e4bcc14e5a37486feb76c11664be
Tanay Topac <tanaytopac@stanford.edu>
2020-09-16 11:21:22 -0700

    changes in commSG temp. comp. algorithm.

--- helpers/proc_tempcomp_helper.py
+++ helpers/proc_tempcomp_helper.py
@@ -9,10 +9,10 @@ class CommSG_Temp_Comp():
     self.ref_temp = ref_temp
 
   def comp_steel (self, temp):
-    e_therm = self.poly_coeffs[0] + self.poly_coeffs[1]*temp + self.poly_coeffs[2]*temp**2 + self.poly_coeffs[3]*temp**3
-    uncert = (temp - self.ref_temp) * self.poly_coeffs[4]
-    lead_influence = (temp - self.ref_temp) * self.poly_coeffs[5]
-    return (e_therm, uncert, lead_influence)
+    e_therm = self.poly_coeffs[0] + self.poly_coeffs[1]*temp + self.poly_coeffs[2]*temp**2 + self.poly_coeffs[3]*temp**3 + (temp-20) * self.poly_coeffs[5]
+    e_therm_ref = self.poly_coeffs[0] + self.poly_coeffs[1]*self.ref_temp + self.poly_coeffs[2]*self.ref_temp**2 + self.poly_coeffs[3]*self.ref_temp**3 + (self.ref_temp-20) * self.poly_coeffs[5]
+    uncert = (temp - 20) * self.poly_coeffs[4]
+    return (e_therm, e_therm_ref, uncert)
 
   def correct_gage_fact (self, temp):
     return self.gage_fact + self.gage_fact_CTE*(temp - self.ref_temp)
@@ -21,11 +21,12 @@ class CommSG_Temp_Comp():
     return (self.substrate_matl_CTE - self.SG_matl_CTE)*(temp - self.ref_temp)*1E6
 
   def compensate (self, readings, temp):
-    e_therm, uncert, lead_influence = self.comp_steel(temp)
+    e_therm, e_therm_ref, uncert = self.comp_steel(temp)
     corr_gage_fact = self.correct_gage_fact(temp)
     corr_matl_dev = self.comp_matl_dev(temp)
-    e_steel = readings*self.gage_fact/corr_gage_fact - (e_therm*self.k_poly/self.gage_fact) - lead_influence
-    e_substrate = e_steel - corr_matl_dev
+    e_steel = readings*self.gage_fact/corr_gage_fact - (e_therm*self.k_poly/self.gage_fact)
+    e_steel_ref = readings[:,0]*self.gage_fact/corr_gage_fact - (e_therm_ref*self.k_poly/self.gage_fact)
+    e_substrate = e_steel - e_steel_ref - corr_matl_dev #Make sure of the sign before corr_matl_dev
     return (e_substrate, uncert)
 
   
