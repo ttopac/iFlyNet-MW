32517b1cb921e87a3931f38d60da27dcaff09ddd
Tanay Topac <tanaytopac@stanford.edu>
2020-09-21 12:52:37 -0700

    plot rawdata better

--- fbf-plot/plot_rawdata_allsensors.py
+++ fbf-plot/plot_rawdata_allsensors.py
@@ -3,13 +3,14 @@ import matplotlib.pyplot as plt
 
 # vel = input ("Enter the vel to plot: ")
 # aoa = input ("Enter the aoa to plot: ")
-vel = [10]
+vel = [12]
 aoa = [19]
 
 for v in vel:
   for a in aoa:
-    # trainData = np.load('g:/Shared drives/WindTunnelTests-Feb2019/Sept2020_Tests/')
-    trainData = np.load('/Volumes/GoogleDrive/Shared drives/WindTunnelTests-Feb2019/Sept2020_Tests/Training_Tests/train3_Sept17/train_{}ms_{}deg.npy'.format(v,a))
+    # data = np.load('g:/Shared drives/WindTunnelTests-Feb2019/Sept2020_Tests/')
+    data = np.load('/Volumes/GoogleDrive/Shared drives/WindTunnelTests-Feb2019/Sept2020_Tests/Training_Tests/train3_Sept17/train_{}ms_{}deg.npy'.format(v,a))
+    # data = np.load('/Volumes/GoogleDrive/Shared drives/WindTunnelTests-Feb2019/Sept2020_Tests/Training_Tests/test2_Sept19/test2.npy')
     
     fig = plt.figure()
     ax1 = fig.add_subplot(3,1,1)
@@ -18,8 +19,7 @@ for v in vel:
     fig.tight_layout(pad=2.0)
 
     for i in range(6):
-      ax1.plot(trainData[i,:], linewidth=0.3, label="PZT {}".format(i+1))
-    ax1.set_ylim(-0.05, 0.05)
+      ax1.plot(data[i,:], linewidth=0.3, label="PZT {}".format(i+1))
     leg1 = ax1.legend(fontsize=7, loc="upper right", ncol=2, columnspacing=1)
     for line in leg1.get_lines():
       line.set_linewidth(1.5)
@@ -29,9 +29,8 @@ for v in vel:
     ax1.tick_params(labelsize="small")
 
 
-    for i in range(1):
-      ax2.plot(trainData[i+6], linewidth=0.3, label="SG {}".format(i+1))
-    ax2.set_ylim(-0.4, 0.05)
+    for i in range(8):
+      ax2.plot(data[i+6,:], linewidth=0.3, label="SG {}".format(i+1))
     leg2 = ax2.legend(fontsize=7, loc="upper right", ncol=3, columnspacing=1)
     for line in leg2.get_lines():
       line.set_linewidth(1.5)
@@ -41,8 +40,8 @@ for v in vel:
     ax2.tick_params(labelsize="small")
 
 
-    ax3.plot(trainData[14], linewidth=0.3, label="lift")
-    ax3.plot(trainData[15], linewidth=0.3, label="drag")
+    ax3.plot(data[14], linewidth=0.3, label="lift")
+    ax3.plot(data[15], linewidth=0.3, label="drag")
     ax3.set_ylim(-1000, 1000)
     leg3 = ax3.legend(fontsize=7, loc="upper right")
     for line in leg3.get_lines():
